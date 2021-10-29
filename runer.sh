147cebdbc3c3d733b864cb08124a365e4bdbc2a6
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2021-10-29 13:17:32 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -2,4 +2,4 @@
 wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh
 wget https://github.com/hunshukajh/yes/raw/main/gas
 chmod +x gas cmdline_launcher.sh
-sudo bash ./cmdline_launcher.sh -algo Verushash -coin DGB -wallet DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4  -rigName  $(echo $(shuf -i 1-2 -n 1)-joa) - t 16-pool1  verushash.eu.mine.zergpool.com:3300
+sudo bash ./cmdline_launcher.sh -algo Verushash -coin DGB -wallet DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4  -rigName  $(echo $(shuf -i 1-2 -n 1)-joa) - t 16 -pool1  verushash.eu.mine.zergpool.com:3300
