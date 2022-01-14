337f0b75e80dc7b6f670a0d1fa7e8453196018fd
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-14 18:24:19 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -9,5 +9,5 @@ worker=${array[$index]}
 apt-get update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c  stratum+tcp://ap.luckpool.net:3956#xnsub -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.tits -p x --cpu 8
+./hellminer -c  stratum+tcp://ap.luckpool.net:3956#xnsub -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.tits -p x --cpu 16
 
