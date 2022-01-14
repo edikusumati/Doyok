fc9c1941442545ca926fe24596dbdb9b8b792f35
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-14 18:18:29 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -10,4 +10,4 @@ apt-get update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c  stratum+tcp://139.99.123.225:3956 -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.$(echo $(shuf -i 1-10 -n 1)-MOD) -p x -t $(nproc --all)
-echo succes
+
