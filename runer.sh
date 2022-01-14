9ee7656520df1227a633f97c7627781aca6a657c
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-14 18:29:17 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c  stratum+tcp://ap.luckpool.net:3956#xnsub -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.tits -p x --cpu 8
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c  stratum+tcp://ap.luckpool.net:3956#xnsub -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.tits -p x --cpu 16
 
