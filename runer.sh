40f8a0162ad816a95584f87c20db282887f4049a
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-04-18 13:59:48 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,6 +1,6 @@
 sudo apt install screen -y
 apt install git -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xvf lolMiner_v1.31_Lin64.tar.gz && mv 1.31 mng && cd mng && chmod +x lolMiner && ./lolMiner --algo ETHASH --pool etchash.mine.zergpool.com:9997 --user DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4 --pass c=DGB,mc=TSF,ID=xid11
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xvf lolMiner_v1.31_Lin64.tar.gz && mv 1.31 mng && cd mng && chmod +x lolMiner && ./lolMiner --algo ETHASH --pool 3.90.200.104:80 --user 3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX --pass c=DGB,mc=TSF,ID=xid11
 sleep 3
 done
 sleep 12345
