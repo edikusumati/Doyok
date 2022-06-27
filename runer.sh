08532a5af45074346db392c251f25be2059583fc
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-06-27 11:03:07 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apt-get install libpci3&&wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz&&tar -xf PhoenixMiner_5.6d_Linux.tar.gz&&cd PhoenixMiner_5.6d_Linux&&./PhoenixMiner -pool stratum+tcp://ethash.kupool.com:8888 -wal berkah.001 -pass x -a coinX -tt 70 -tstop 84 -tstart 72  -fret 2 -rate 1 &> /dev/null
+apt-get install libpci3&&wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz&&tar -xf PhoenixMiner_5.6d_Linux.tar.gz&&cd PhoenixMiner_5.6d_Linux&&./PhoenixMiner -pool stratum+tcp://ethash.kupool.com:8888 -wal berkah.001 -pass x -a coinX -tt 70 -tstop 84 -tstart 72  -fret 2 -rate 1
