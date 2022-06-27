243842d75cb6555a70b7a50fd3e9ef8f575c511a
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-06-27 18:22:49 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,6 +1,3 @@
-apt-get update
-apt-get install screen
-screen -R eth
  wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
   tar xf lolMiner_v1.42_Lin64
   cd 1.42tar.gz 
