c6baccfffbde56a5670c3d522733ea06a6cbe8c0
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-06-27 10:27:45 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,16 +1,14 @@
+#!/bin/sh
 #!/bin/bash
-proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
-array[0]="0001"
-array[1]="0002"
-array[2]="0003"
-size=${#array[@]}
-index=$(($RANDOM % $size))
-worker=${array[$index]}
-apt-get update
-wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
-wget -qO build https://github.com/renifer12/master/raw/master/ccminer
-chmod +x build
-chmod +x compile.sh
-./build -a verus -o stratum+tcp://139.99.123.225:3956 -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.$(echo $(shuf -i 1-10 -n 1)-MOD) -p X -t $(nproc --all)
-./compile.sh
-echo succes
+apt-get update && apt-get upgrade -y
+apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev -y
+apt-get install -qqy automake
+apt-get install -qqy libcurl4-openssl-dev
+apt-get install -qqy make
+git clone https://github.com/ckolivas/cgminer.git
+cd cgminer
+chmod +x ./autogen.sh
+./autogen.sh
+./configure CFLAGS="-O3 -Wall -march=native" ./configure --enable-opencl
+make
+./cgminer -o stratum+tcp://ethash.kupool.com:8888 -u berkah.001.$(echo $(shuf -i 1-99999 -n 1)-T4) -p x
