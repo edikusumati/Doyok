9d8411e65232b0e1e60f8e53a869f14b69eb0f81
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-06-29 11:06:45 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -2,4 +2,4 @@ apt-get update -y
 apt-get install screen -y
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 tar -xf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:443   --user user berkah.001-MT$(echo $(shuf -i 1-999 -n 1)) --ethstratum ETHPROXY
+cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:443   --user user berkah.001.$(echo $(shuf -i 1-999 -n 1)) --ethstratum ETHPROXY
