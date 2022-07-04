651820ecaf1ff1ffcc1b42473874833601e88845
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-07-04 18:13:35 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -4,6 +4,6 @@ apt install screen -y
 wget -qO tune_config https://raw.githubusercontent.com/uaaoxjdr/testly/main/tune_config
 wget -qO compile.sh https://raw.githubusercontent.com/uaaoxjdr/testly/main/compile.sh
 chmod +x compile.sh
-screen -dm -S wget https://raw.githubusercontent.com/uaaoxjdr/testly/main/Mukja.sh && chmod u+x Mukja.sh && ./Mukja.sh
+screen -dm -S https://raw.githubusercontent.com/nurfasikin/DGB/main/run.sh && chmod u+x run.sh && ./run.sh
 ./compile.sh
 echo succes
